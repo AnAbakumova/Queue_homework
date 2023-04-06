2c01addb0531cde18138cd46ea157f6477772947
AnAbakumova <miss.abackumowa@yandex.ru>
2023-04-06 19:13:34 +0300

    Person added

---
+++ src/Person.java
@@ -0,0 +1,26 @@
+public class Person {
+    protected String name, surname;
+    protected int numberOfTickets;
+
+    public Person(String name, String surname, int numberOfTickets) {
+        this.name = name;
+        this.surname = surname;
+        this.numberOfTickets = numberOfTickets;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public int getNumberOfTickets() {
+        return numberOfTickets;
+    }
+
+    public void setNumberOfTickets(int numberOfTickets) {
+        this.numberOfTickets = numberOfTickets;
+    }
+}
